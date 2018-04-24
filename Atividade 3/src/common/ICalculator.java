13c6dcdbca01b85e6b454318bcb2a31abd6368f8
Tatiana Viana <tvp@cin.ufpe.br>
2018-04-23 22:42:36 -0300

    Added Calculator class and interface

---
+++ Atividade 3/src/common/ICalculator.java
@@ -0,0 +1,6 @@
+package common;
+
+public interface ICalculator {
+	public float add(float a, float b) throws Throwable;
+	public float sub(float a, float b) throws Throwable;
+}
---
+++ Atividade 3/src/server/Calculator.java
@@ -0,0 +1,11 @@
+package server;
+
+public class Calculator {
+	public float add(float a, float b) {
+		return a + b;
+	}
+	
+	public float sub(float a, float b) {
+		return a - b;
+	}
+}
