010b32f41cf8f037a17939bfb46c0febc7cebe1c
Tatiana Viana <tvp@cin.ufpe.br>
2018-04-23 23:53:12 -0300

    Added Marshaller

---
+++ Atividade 3/src/common/Marshaller.java
@@ -0,0 +1,24 @@
+package common;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Marshaller {
+	public byte[] marshall(Message msg) throws IOException, InterruptedException {
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
+		objectStream.writeObject(msg);
+		
+		return byteStream.toByteArray();
+	}
+	
+	public Message unmarshall(byte[] msg) throws IOException, InterruptedException, ClassNotFoundException {
+		ByteArrayInputStream byteStream = new ByteArrayInputStream(msg);
+		ObjectInputStream objectStream = new ObjectInputStream(byteStream);
+		
+		return (Message) objectStream.readObject();
+	} 
+}
