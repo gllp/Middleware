679c5909bc57f62be71e773c194877fa52b283c8
Tatiana Viana <tvp@cin.ufpe.br>
2018-04-25 13:58:32 -0300

    Added Invoker

---
+++ Atividade 3/src/server/Invoker.java
@@ -0,0 +1,58 @@
+package server;
+
+import java.io.IOException;
+
+import common.Marshaller;
+import common.Message;
+import common.MessageBody;
+import common.MessageHeader;
+import common.Termination;
+import client.ClientProxy;
+import client.RequestBody;
+import client.RequestHeader;
+
+public class Invoker {
+	public void invoke(ClientProxy clientProxy) throws IOException, Throwable {
+		ServerRequestHandler srh = new ServerRequestHandler(clientProxy.getPort());
+		byte[] msgToBeUnmarshalled = null;
+		byte[] msgMarshalled = null;
+		Message msgUnmarshalled = null;
+		Marshaller marshaller = new Marshaller();
+		Termination ter = new Termination();
+		
+		Calculator rObj = new Calculator();
+		
+		while(true) {
+			msgToBeUnmarshalled = srh.receive();
+			msgUnmarshalled = marshaller.unmarshall(msgToBeUnmarshalled);
+			
+			switch(msgUnmarshalled.getBody().getRequestHeader().getOperation()) {
+				case "add":
+					float addP1 = (float) msgUnmarshalled.getBody().getRequestBody().getParameters().get(0);
+					float addP2 = (float) msgUnmarshalled.getBody().getRequestBody().getParameters().get(1);
+					ter.setResult(rObj.add(addP1, addP2));
+					
+					Message addMsgToBeMarshalled = new Message(new MessageHeader("protocolo", 0, false, 0, 0), 
+							new MessageBody(null, null, new ReplyHeader("", 0, 0), new ReplyBody(ter.getResult())));
+					
+					msgMarshalled = marshaller.marshall(addMsgToBeMarshalled);
+					
+					srh.send(msgMarshalled);
+					return;
+					
+				case "sub":
+					float subP1 = (float) msgUnmarshalled.getBody().getRequestBody().getParameters().get(0);
+					float subP2 = (float) msgUnmarshalled.getBody().getRequestBody().getParameters().get(1);
+					ter.setResult(rObj.sub(subP1, subP2));
+					
+					Message subMsgToBeMarshalled = new Message(new MessageHeader("protocolo", 0, false, 0, 0), 
+							new MessageBody(null, null, new ReplyHeader("", 0, 0), new ReplyBody(ter.getResult())));
+					
+					msgMarshalled = marshaller.marshall(subMsgToBeMarshalled);
+					
+					srh.send(msgMarshalled);
+					return;
+			}
+		}
+	}
+}
