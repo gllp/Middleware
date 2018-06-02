dcd2bdc9f2971726efd0e8810640973ceec933b7
Tatiana Viana <tvp@cin.ufpe.br>
2018-06-02 10:36:17 -0300

    Added ClientRequest Handler

---
+++ Projeto/src/infrastructure/ClientRequestHandler.java
@@ -0,0 +1,63 @@
+package infrastructure;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientRequestHandler {
+	private String host;
+	private int port;
+	private int sentMessageSize;
+	private int receiveMessageSize;
+	
+	private boolean expectedReply;
+	
+	private Socket clientSocket;
+	
+	private DataOutputStream outToServer;
+	private DataInputStream inFromServer;
+	
+	public ClientRequestHandler(String host, int port, boolean expectedReply) {
+		this.host = host;
+		this.port = port;
+		this.expectedReply = expectedReply;
+	}
+	
+	public void send(byte[] msg) throws IOException {
+		sentMessageSize = msg.length;
+		
+		clientSocket = new Socket(host, port);
+		
+		inFromServer = new DataInputStream(clientSocket.getInputStream());
+		outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		
+		outToServer.writeInt(sentMessageSize);
+		outToServer.write(msg, 0, sentMessageSize);
+		
+		if(!this.expectedReply) {
+			clientSocket.close();
+			inFromServer.close();
+			outToServer.close();
+		}
+	} 
+	
+	public byte[] receive() throws IOException {
+		receiveMessageSize = inFromServer.readInt();
+		
+		byte[] data = new byte[receiveMessageSize];
+		inFromServer.readFully(data, 0, receiveMessageSize);
+		
+		clientSocket.close();
+		inFromServer.close();
+		outToServer.close();
+		
+		System.out.println("Fechou client socker");
+		
+		return data;
+	}
+	
+	public boolean isExpectedReply() {
+		return expectedReply;
+	}
+}
